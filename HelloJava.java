ad50626c0b98c5bb3c7f189b31784f77b5103d38
sonali <waghmaress162000@gmail.com>
2024-04-27 02:02:21 +0530

    changed output Hello from C-DAC ACTS

--- HelloJava.java
+++ HelloJava.java
@@ -1,5 +1,5 @@
 public class HelloJava {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello from C-DAC ACTS"); 
     }
 }
